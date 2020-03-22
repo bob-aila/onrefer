038633bfd36ed997a3b6d12acbda9da9fa417259
bob-aila <61519243+bob-aila@users.noreply.github.com>
2020-03-22 12:57:55 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,9 @@
 <?php
 require('database.php');
 $id=$_REQUEST['id'];
-$query = "DELETE FROM admin WHERE id=$id"; 
+$query = "DELETE FROM registered WHERE id=$id"; 
+$result = mysqli_query($con,$query) or die ( mysqli_error());
+$query = "DELETE FROM users WHERE id=$id"; 
 $result = mysqli_query($con,$query) or die ( mysqli_error());
 header("Location: summary.php"); 
 ?>
