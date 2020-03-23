785a4426d19712cb98787532126b9d83425089f3
bob-aila <61519243+bob-aila@users.noreply.github.com>
2020-03-23 12:13:48 +0300

    Update and rename admlogin.html to admlogin.php

--- admlogin.php
+++ admlogin.php
@@ -71,8 +71,49 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body oncontextmenu="return false" onselectstart="return false" on ondragstart="return false">
+  <?php
+session_start();
+require('database.php');
+// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+if ( isset($_POST['username'], $_POST['password']) ) {
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT id, password FROM admin WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+     
+    if ($stmt->num_rows > 0) {
+	$stmt->bind_result($id, $password);
+	$stmt->fetch();
+	// Account exists, now we verify the password.
+	// Note: remember to use password_hash in your registration file to store the hashed passwords.
+    
+    if ($_POST['password'] === $password) {
+        //will use this if the password is not encripted //if (password_verify($_POST['password'], $password)){
+		// Verification success! User has loggedin!
+		// Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
+		session_regenerate_id();
+		$_SESSION['loggedin'] = TRUE;
+		$_SESSION['name'] = $_POST['username'];
+		$_SESSION['id'] = $id;
+        //This redirects us to the homepage after successfull login
+		header('Location: admin.php');
+	} else {
+        echo "<script type='text/javascript'>alert('wrong username');</script>";
+	}
+} else {
+        echo "<script type='text/javascript'>alert('wrong password');</script>";
+}
+
+	$stmt->close();
+}
+} 
+
+?>
 <h2><center>Admin Login Form</center></h2>
-<center><form action="oath.php" method="post">
+<center><form method="post" action="" enctype="multipart/form-data".<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>>  
   <div class="imgcontainer">
     <img src="images/logo.png" alt="Avatar" class="avatar">
   </div>
