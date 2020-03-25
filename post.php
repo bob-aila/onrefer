b16f6aa6b9af591a5776d44416264afe497a617b
bob-aila <61519243+bob-aila@users.noreply.github.com>
2020-03-26 00:17:42 +0300

    Add files via upload
    communications

--- post.php
+++ post.php
@@ -1,9 +1,11 @@
 <?
 session_start();
+$nise=$_SESSION['name'];
 if(isset($_SESSION['name'])){
-    $text = $_POST['text']; 
-    $fp = fopen("log.html", 'a');
-    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
-    fclose($fp);
+	$text = $_POST['text'];
+	
+	$fp = fopen("log.html", 'a');
+	fwrite($fp, "<div class='msgln'><b>".$_SESSION['name']."</b>: <br>".stripslashes(htmlspecialchars($text))."<br>(".date("g:i A").")<br></div>");
+	fclose($fp);
 }
 ?>
