57fe5f99fecd6054b0eb4b0163b1b124bb3b9b83
bob-aila <61519243+bob-aila@users.noreply.github.com>
2020-03-26 23:06:06 +0300

    Update post.php

--- post.php
+++ post.php
@@ -1,9 +1,9 @@
+
+
 <?
 session_start();
-$nise=$_SESSION['name'];
-if(isset($_SESSION['name'])){
+if(isset($_POST['submitmsg'])){
 	$text = $_POST['usermsg'];
-	
 	$fp = fopen("log.html", 'a');
 	fwrite($fp, "<div class='msgln'><b>".$_SESSION['name']."</b>: <br>".stripslashes(htmlspecialchars($text))."<br>(".date("g:i A").")<br></div>");
 	fclose($fp);
