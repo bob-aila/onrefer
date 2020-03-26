6f350169d36e568810257c58030fa125598bc65f
bob-aila <61519243+bob-aila@users.noreply.github.com>
2020-03-26 19:03:51 +0300

    Update post.php

--- post.php
+++ post.php
@@ -2,10 +2,11 @@
 session_start();
 $nise=$_SESSION['name'];
 if(isset($_SESSION['name'])){
-	$text = $_POST['text'];
+	$text = $_POST['usermsg'];
 	
 	$fp = fopen("log.html", 'a');
 	fwrite($fp, "<div class='msgln'><b>".$_SESSION['name']."</b>: <br>".stripslashes(htmlspecialchars($text))."<br>(".date("g:i A").")<br></div>");
 	fclose($fp);
+header('location: chat2.php');
 }
 ?>
