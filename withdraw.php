620847b183727681eb62e42dab267fc544c39364
bob-aila <61519243+bob-aila@users.noreply.github.com>
2020-04-09 14:55:53 +0300

    Update withdraw.php

--- withdraw.php
+++ withdraw.php
@@ -1,6 +1,7 @@
 <?php
 require('database.php');
-$check = "SELECT * FROM users WHERE username='$_POST[username]' AND password='$_POST[password]' LIMIT 1";
+$pass=hash("sha256", $_POST['password']);
+$check = "SELECT * FROM users WHERE username='$_POST[username]' AND password='$pass' LIMIT 1";
 $results = mysqli_query($con, $check);
 if (mysqli_num_rows($results) == 1) {
 $check = "SELECT * FROM registered WHERE username='$_POST[username]' AND contact='$_POST[phone]' LIMIT 1";
